c79818599e28368dcdada9abeadb78cb2c34f46e
durga1808 <durgalakshmi3713@gmail.com>
2023-11-28 16:40:29 +0530

    added all the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { VideosComponent } from './videos/videos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: MainContentComponent },
+  { path : '', component: VideosComponent}
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { VideosComponent } from './videos/videos.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    VideosComponent,
+    MainContentComponent,
+         
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+       
+       RouterModule.forRoot([
+        { path: 'videos', component: VideosComponent }, 
+            ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
