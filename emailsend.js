9a28f3727c89d1efa15bc6590255a716e707c1c5
sharanyajune7 <sharanya.june7@gmail.com>
2024-01-04 23:57:42 +0530

    Updated Registration Page

---
+++ emailsend.js
@@ -0,0 +1,26 @@
+function sendMail() {
+
+    var userName = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    var subject = document.getElementById('subject').value;
+    
+    var messageBody = "Name :-" + userName + "  Mail Address:- " + email + "   message :- " + message
+
+    Email.send({
+        SecureToken : "9f4c4f41-276b-4c7d-bc20-1c28be67816d",
+        To : "sharanya.june7@gmail.com",
+        From : "surendhar030298@gmail.com",
+        Subject : subject,
+        Body : messageBody
+        }).then(
+            message => {
+                if(message=='OK'){
+                    swal("We will get back to you!", "success");
+                }
+                else{
+                    swal("Error", "error");
+                }
+            }
+        );
+}
