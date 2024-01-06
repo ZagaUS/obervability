cd3fd3022b7a58201adea3fbb3e5b4a592903d81
sharanyajune7 <sharanya.june7@gmail.com>
2024-01-06 22:36:36 +0530

    Added Email validation

--- registration.js
+++ registration.js
@@ -3,11 +3,27 @@ function SubmitForm(){
         name: $("#name").val(),
         phoneNo: $("#phoneNo").val(),
         location: $("#location").val(),
+        role: $("#role").val(),
+        department: $("#department").val(),
+        workPhone: $("#workPhone").val(),
         // address: $("#address").val(),
         companyEmail: $("#companyEmail").val(),
         companyName: $("#companyName").val(),
 
     };
+
+    // private Long phoneNo;
+    // private String companyEmail;
+    // private String companyName;
+    // private String location;
+    // private String role;
+    // private String department;
+    // private Long workPhone;
+    // // private String address;
+    // private String username;
+    // private String password;
+    // private LocalDate todayDate;
+    // private LocalDate expiryDate;
 
     $.ajax({
         type: "POST",
@@ -19,7 +35,7 @@ function SubmitForm(){
         success: function (response) {
             // console.log("User registered successfully:", response);
             // console.log(JSON.stringify(response));
-            sendMailRegistration(response.name,response.username,response.password,response.companyEmail)
+            sendMailRegistration(response.name,response.username,response.password,response.companyEmail,response.expiryDate)
             // Handle success, e.g., display a success message to the user
             // alert("user registered successfully")
             // window.location.href = "trial.html";
@@ -28,13 +44,22 @@ function SubmitForm(){
                 text: "Registration succefully done!",
                 type: "success"
             }).then(okay => {
-                if(okay){
-                    window.location.href = "trial.html";
-                }
+                // if(okay){
+                //     window.location.href = "trial.html";
+                // }
             })
         },
         error: function (error) {
             console.error("Error registering user:", error);
+            swal({
+                title: "Failed",
+                text: " User Already Existed",
+                type: "error"
+            }).then(okay => {
+                if(okay){
+                    window.location.href = "trial.html";
+                }
+            })
             // Handle error, e.g., display an error message to the user
         }
     })
@@ -50,7 +75,7 @@ function navigatePage() {
 
 
 
-function sendMailRegistration(name,username,password,emailID) {
+function sendMailRegistration(name,username,password,emailID,expiryDate) {
 
     Email.send({
         SecureToken : "9f4c4f41-276b-4c7d-bc20-1c28be67816d",
@@ -399,7 +424,7 @@ function sendMailRegistration(name,username,password,emailID) {
                             <td>
                             <![endif]-->
                                 <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="640" style="margin: auto;" class="contentMainTable">
-                                    <tr class="wp-block-editor-spacerblock-v1"><td style="background-color:#F5F6F8;line-height:50px;font-size:50px;width:100%;min-width:100%">&nbsp;</td></tr><tr class="wp-block-editor-headingblock-v1"><td valign="top" style="background-color:#ffffff;display:block;padding-top:64px;padding-right:32px;padding-bottom:32px;padding-left:32px;text-align:center"><p style="font-family:Open Sans, sans-serif;text-align:center;line-height:36.80px;font-size:32px;background-color:#ffffff;color:#000000;margin:0;word-break:normal" class="heading1">Thanks for choosing us</p></td></tr><tr class="wp-block-editor-paragraphblock-v1"><td valign="top" style="padding:0px 32px 32px 32px;background-color:#ffffff"><p class="paragraph" style="font-family:Open Sans, sans-serif;text-align:left;line-height:30.00px;font-size:15px;margin:0;color:#5f5f5f;word-break:normal">Name: ${name}<br>Username: ${username}<br>Password: ${password}<br>Expiry date: 31-Jan-2024</p></td></tr><tr><td style="padding-top:20px;padding-left:0;padding-right:0;padding-bottom:20px;background-color:#FFFFFF"><table role="presentation" class="multi-column" style="width:640px;border-collapse:collapse !important" cellpadding="0" cellspacing="0"><tbody><tr style="padding-top:20px;padding-left:0;padding-right:0;padding-bottom:20px" class="wp-block-editor-onecolumnsblock-v1"><td style="width:640px;float:left" class="wp-block-editor-column single-column"><table role="presentation" align="left" border="0" class="single-column" width="640" style="width:640px;float:left;border-collapse:collapse !important" cellspacing="0" cellpadding="0"><tbody><tr class="wp-block-editor-buttonblock-v1" align="center"><td style="background-color:#FFFFFF;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px;width:100%" valign="top"><table role="presentation" cellspacing="0" cellpadding="0" class="button-table"><tbody><tr><td valign="top" class="button-td button-td-primary" style="cursor:pointer;border:none;border-radius:4px;background-color:#5457ff;font-size:16px;font-family:Open Sans, sans-serif;width:fit-content;letter-spacing:0;color:#ffffff"><a style="color:#ffffff" href="https://zaga-observability.zagaopensource.com/">
+                                    <tr class="wp-block-editor-spacerblock-v1"><td style="background-color:#F5F6F8;line-height:50px;font-size:50px;width:100%;min-width:100%">&nbsp;</td></tr><tr class="wp-block-editor-headingblock-v1"><td valign="top" style="background-color:#ffffff;display:block;padding-top:64px;padding-right:32px;padding-bottom:32px;padding-left:32px;text-align:center"><p style="font-family:Open Sans, sans-serif;text-align:center;line-height:36.80px;font-size:32px;background-color:#ffffff;color:#000000;margin:0;word-break:normal" class="heading1">Thanks for choosing us</p></td></tr><tr class="wp-block-editor-paragraphblock-v1"><td valign="top" style="padding:0px 32px 32px 32px;background-color:#ffffff"><p class="paragraph" style="font-family:Open Sans, sans-serif;text-align:left;line-height:30.00px;font-size:15px;margin:0;color:#5f5f5f;word-break:normal">Name: ${name}<br>Username: ${username}<br>Password: ${password}<br>Expiry date: ${expiryDate}</p></td></tr><tr><td style="padding-top:20px;padding-left:0;padding-right:0;padding-bottom:20px;background-color:#FFFFFF"><table role="presentation" class="multi-column" style="width:640px;border-collapse:collapse !important" cellpadding="0" cellspacing="0"><tbody><tr style="padding-top:20px;padding-left:0;padding-right:0;padding-bottom:20px" class="wp-block-editor-onecolumnsblock-v1"><td style="width:640px;float:left" class="wp-block-editor-column single-column"><table role="presentation" align="left" border="0" class="single-column" width="640" style="width:640px;float:left;border-collapse:collapse !important" cellspacing="0" cellpadding="0"><tbody><tr class="wp-block-editor-buttonblock-v1" align="center"><td style="background-color:#FFFFFF;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px;width:100%" valign="top"><table role="presentation" cellspacing="0" cellpadding="0" class="button-table"><tbody><tr><td valign="top" class="button-td button-td-primary" style="cursor:pointer;border:none;border-radius:4px;background-color:#5457ff;font-size:16px;font-family:Open Sans, sans-serif;width:fit-content;letter-spacing:0;color:#ffffff"><a style="color:#ffffff" href="https://zaga-observability.zagaopensource.com/">
             <table role="presentation">
             <tbody><tr>
               <!-- Top padding -->
